1dc1b12592b12569a1e2f10dafd4bb6b2ed8b88e
Aldryan <aldryan.putra@checkproof.com>
2025-12-22 19:45:39 +0700

    add analytics

--- index.tsx
+++ index.tsx
@@ -1,13 +1,12 @@
+import { bootstrapApplication } from "@angular/platform-browser";
+import { AppComponent } from "./src/app.component";
+import { provideZonelessChangeDetection } from "@angular/core";
+import { inject } from "@vercel/analytics";
 
-
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './src/app.component';
-import { provideZonelessChangeDetection } from '@angular/core';
+inject();
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideZonelessChangeDetection()
-  ]
-}).catch(err => console.error(err));
+  providers: [provideZonelessChangeDetection()],
+}).catch((err) => console.error(err));
 
 // AI Studio always uses an `index.tsx` file for all project types.
